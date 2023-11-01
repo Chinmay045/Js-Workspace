1feab3bac43170c34d1fabfeb1247be59315377a
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 14:29:52 +0530

    add Js example of using empty Array

--- 0C/index.js
+++ 0C/index.js
@@ -1,2 +1,8 @@
-let southAsia = ['India' , 'Pakistan' , 'Bangladesh'];
+let southAsia = ['India', 'Pakistan', 'Bangladesh'];
 console.log(southAsia);
+
+let SouthAsia = [];
+SouthAsia[0] = 'India';
+SouthAsia[1] = 'Myanmar';
+SouthAsia[2] = "Bangladesh";
+console.log(SouthAsia);
