893fd126e39161eaf2e1175b1b9f523c2ddaea2f
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 10:05:52 +0530

    add Js example on conditional statements with non-boolean

--- 0B/index.js
+++ 0B/index.js
@@ -1,17 +1,25 @@
 
-let flag = true;
-if (flag) {
-    console.log("Salute, Salute, Salute");
-}
+// let flag = true;
+// if (flag) {
+//     console.log("Salute, Salute, Salute");
+// }
 
-if (5 > 6) {
-    console.log("Do Something!");
-}
-else if (7 > 9) {
-    console.log("Do Something Something");
-}
-else {
-    console.log("Do anything");
-}
+// if (5 > 6) {
+//     console.log("Do Something!");
+// }
+// else if (7 > 9) {
+//     console.log("Do Something Something");
+// }
+// else {
+//     console.log("Do anything");
+// }
 
+let flag = null;
+
+if(flag){
+    console.log("Flag is Truthy");
+}
+else{
+    console.log("Flag is Falsy");
+}
 
