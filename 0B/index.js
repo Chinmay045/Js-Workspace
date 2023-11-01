47d6911e5a672c6454ff908e290bf7c3b8b5206a
Chinmay <chinz.sandur07@gmail.com>
2023-11-01 10:03:12 +0530

    add Js example on conditional statements

---
+++ 0B/index.js
@@ -0,0 +1,17 @@
+
+let flag = true;
+if (flag) {
+    console.log("Salute, Salute, Salute");
+}
+
+if (5 > 6) {
+    console.log("Do Something!");
+}
+else if (7 > 9) {
+    console.log("Do Something Something");
+}
+else {
+    console.log("Do anything");
+}
+
+
