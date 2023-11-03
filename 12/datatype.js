27dfa0496cafac317bcd7ba62847f52e325ed64a
Chinmay <chinz.sandur07@gmail.com>
2023-11-03 14:16:08 +0530

    add example of object and array data type

--- 12/datatype.js
+++ 12/datatype.js
@@ -11,3 +11,11 @@ let lastName = "Johnson";
 let x = true;
 let y = false;
 
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
