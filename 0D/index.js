c45b4adc0dea9770aadacc66286e4708f3e9e9da
Chinmay <chinz.sandur07@gmail.com>
2023-11-03 09:51:20 +0530

    add example of callback functions

--- 0D/index.js
+++ 0D/index.js
@@ -23,3 +23,39 @@ console.log(addMethod);
 console.log(addTwoNumbers.name);
 
 console.log(addMethod.name);
+
+//This is Aam Zindagi
+
+const add = (x, y) => {
+    console.log(`Sum of two numbers is : ${x + y}`);
+}
+
+add(5, 6);
+
+//Thhis is Mentos Zindagi
+
+const sum = (param) => {
+    param(5, 6);
+    console.log(`Finished adding two numbers`);
+}
+
+const add = (x, y) => {
+    console.log(`Sum of two nhumbers is :${x + y}`);
+}
+
+sum(add);
+
+//Mentos Zindagi with a twist
+const sum = (param, a, b) => {
+    param(a, b);
+    console.log(`Finished adding two numbers`);
+}
+
+const add = (x, y) => {
+    console.log(`Sum of two numbers is : ${x + y}`);
+}
+
+sum(add, 10, 15);
+
+
+
