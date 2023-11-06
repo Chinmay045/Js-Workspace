8a5461874e12d639e393c7416fd11be2034eaea9
Chinmay <chinz.sandur07@gmail.com>
2023-11-06 14:57:32 +0530

    add example for Factory using get and set methods

--- 0E/index.js
+++ 0E/index.js
@@ -1,9 +1,9 @@
 let dog = {
-    breed: 'Golden Retrevier' ,
+    breed: 'Golden Retrevier',
     color: 'Orange',
     weight: 18,
-    'responds to' : ['wistle', 'clap', 'Throw ball'],
-    tag_no:776
+    'responds to': ['wistle', 'clap', 'Throw ball'],
+    tag_no: 776
 }
 
 let dogColor = dog.color;
@@ -44,36 +44,36 @@ delete baby['baby duck'];
 console.log(baby); //Prints the whole object
 
 const actions = {
-    sayHello: function(){
+    sayHello: function () {
         console.log("Hello Everyone");
     },
 
-    sayNamaste: function(person) {
+    sayNamaste: function (person) {
         console.log(`Namaste ${person}, How are you ?`);
     },
-    add: function (num1,num2) {
+    add: function (num1, num2) {
         return num1 + num2;
     }
 }
 
 actions.sayHello();
 actions.sayNamaste('Chinmay');
-console.log(`Sum of two numbers is :${actions.add(10,15)}`);
+console.log(`Sum of two numbers is :${actions.add(10, 15)}`);
 
 let profile = {
     fullName: 'Chinmay KS',
-    status:'Single',
+    status: 'Single',
     children: 0,
     parents: ['Kantharaj', 'Rashmi'],
     salaried: true
 }
 
-const modifyName = function(obj, changedName) {
+const modifyName = function (obj, changedName) {
     obj.fullName = changedName;
 }
 
 const addChild = (obj) => {
-    obj.children = obj.children +1;
+    obj.children = obj.children + 1;
 }
 
 modifyName(profile, 'CKS');
@@ -93,17 +93,17 @@ let baby = {
     sheep: 'lamb'
 }
 
-for(let key in baby){
+for (let key in baby) {
     console.log(`Child of ${key} is ${baby[key]}`);
 }
 
 //Hybrid Objects
 let dog = {
-    breed: 'Golden Retrevier' ,
+    breed: 'Golden Retrevier',
     color: 'Orange',
     weight: 18,
 
-    hungry(){
+    hungry() {
         console.log("Waggig the tail");
     },
 
@@ -116,15 +116,15 @@ dog.hungry();
 console.log(dog.getInfo());
 
 let dog = {
-    breed: 'Golden Retrevier' ,
+    breed: 'Golden Retrevier',
     color: 'Orange',
     weight: 18,
 
-    hungry: () =>{
+    hungry: () => {
         console.log("Waggig the tail");
     },
 
-    getInfo: () =>{
+    getInfo: () => {
         return `My dog is a ${this.breed},is  ${this.color} in color and weighs ${this.weight} kilos`
     }
 }
@@ -138,18 +138,18 @@ console.log(dog.getInfo()); //My dog is a undefined,is  undefined in color and w
 //Get methods
 
 let dog = {
-    _breed: 'Golden Retrevier' ,
+    _breed: 'Golden Retrevier',
     color: 'Orange',
     _weight: 18,
 
-    get breed(){
+    get breed() {
         return this._breed;
     },
-    get weight(){
+    get weight() {
         return this._weight;
     },
 
-    hungry(){
+    hungry() {
         console.log("Waggig the tail");
     },
 
@@ -169,32 +169,32 @@ console.log(dog.showInfo());
 
 
 let dog = {
-    _breed: 'Golden Retrevier' ,
+    _breed: 'Golden Retrevier',
     color: 'Orange',
     _weight: 18,
 
-    get breed(){
+    get breed() {
         return this._breed;
     },
-    get weight(){
+    get weight() {
         return this._weight;
     },
 
-    set breed(newBreed){
-        if(typeof newBreed == 'string'){
+    set breed(newBreed) {
+        if (typeof newBreed == 'string') {
             this._breed = newBreed;
         }
     },
 
-    set weight(num){
-        if(typeof num == 'number' && num > 0){
+    set weight(num) {
+        if (typeof num == 'number' && num > 0) {
             this._weight = num;
         }
-        else{
+        else {
             console.log("Please enter a valid number above zero");
         }
     }
-   
+
 }
 
 
@@ -213,23 +213,55 @@ console.log(`Dog Color after :${dog.color}`);
 
 
 //Factory
-const dogFactory = (breed, weight, color, violent) =>{
+const dogFactory = (breed, weight, color, violent) => {
     return {
         _breed: breed,
-        _weight:weight,
-        _color:color,
-        _violent:violent,
+        _weight: weight,
+        _color: color,
+        _violent: violent,
 
-        showInfo(){
+        showInfo() {
             console.log(`My dog is a ${this._breed}, whose color is ${this._color} & it weighs ${this._weight} kilos`);
 
-            this._violent ? console.log("It is ferocious \n") :console.log("It is sweet \n");;
+            this._violent ? console.log("It is ferocious \n") : console.log("It is sweet \n");
+        },
+        get breed() {
+            return this._breed;
+        },
+        get weight() {
+            return this._weight;
+        },
+        get color() {
+            return this._color;
+        },
+        get violent() {
+            return this._violent;
+        },
+
+        set breed(br) {
+            if (typeof br == 'string') {
+                this._breed = br;
+            }
+        },
+
+        set weight(wei) {
+            (typeof wei == 'number' && wei > 0) ?
+                this._weight = wei : console.log("Please enter the valid number");
+        },
+
+        set color(col) {
+            if (typeof col == 'string') this._color = col;
+        },
+        set violent(vio) {
+            (typeof vio == 'boolean') ?
+                this._violent = vio : console('Enter valid boolean true or false');
         }
     }
 }
 
-let d1 = dogFactory('Labrardor' ,18, 'Orange', false);
-d1.showInfo();
+    let d1 = dogFactory('Labrardor', 18, 'Orange', false);
+    d1.showInfo();
 
-let d2 = dogFactory('German Sheapered', 22, 'Black', true);
-d2.showInfo();
+    let d2 = dogFactory('German Sheapered', 22, 'Black', true);
+    d2.showInfo();
+
