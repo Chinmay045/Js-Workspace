2f913c84d84e4b852144c3138f45bbe84ad4d403
Chinmay <chinz.sandur07@gmail.com>
2023-11-06 10:12:57 +0530

    add example of Function using return

--- 13/functions.js
+++ 13/functions.js
@@ -4,3 +4,11 @@ function myFunction(p1, p2) {
 
   const Mul = myFunction(3 ,6);
   console.log(Mul);
+
+  // Function is called, the return value will end up in x
+let x = myFunction(4, 3);
+
+function myFunction(a, b) {
+// Function returns the product of a and b
+  return a * b;
+}
