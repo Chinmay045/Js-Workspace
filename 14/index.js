34c447fbff2bb8ae6c713f99226b0287cd576ac9
Chinmay <chinz.sandur07@gmail.com>
2023-11-06 10:51:27 +0530

    add example of events injs

--- 14/index.js
+++ 14/index.js
@@ -9,3 +9,35 @@ const person = {
 };
 
 console.log(person);
+
+const person = {
+    firstName: "John",
+    lastName : "Doe",
+    id       : 5566,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  };
+
+  console.log(person);
+
+  "use strict";
+
+function show() {
+    console.log(this === window);
+}
+// window.show();
+
+let sh = show();
+console.log(sh);
+
+let car = {
+    brand: 'Honda',
+    getBrand: function () {
+        return this.brand;
+    }
+}
+
+console.log(car.getBrand()); // 
+let brand = car.getBrand;
+console.log(brand());
