4f667f09cef0ee44fcb4b6900af63e343b5fff49
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 14:05:11 +0530

    add example for match

--- 18/index.js
+++ 18/index.js
@@ -13,3 +13,7 @@ console.log(search);
 let text = "Hello world, welcome to the universe.";
 let end = text.endsWith("welcome", 20);
 console.log(end);
+
+let text = "The rain in SPAIN stays mainly in the plain";
+let match = text.match("ain");
+console.log(match);
