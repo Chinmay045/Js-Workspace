da2d2cf961010de477cbed29cc696680d1481104
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 13:48:48 +0530

    add example for strings length

--- 16/index.js
+++ 16/index.js
@@ -1,3 +1,7 @@
 let answer1 = "It's alright";
 let answer2 = "He is called 'Johnny'";
 let answer3 = 'He is called "Johnny"';
+
+let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let length = text.length;
+console.log(length);
