2cd070170d34eecc9744ac7485f37668981a835f
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 14:04:07 +0530

    add example for endswith

--- 18/index.js
+++ 18/index.js
@@ -9,3 +9,7 @@ console.log(index);
 let text = "Please locate where 'locate' occurs!";
 let search = text.search("locate");
 console.log(search);
+
+let text = "Hello world, welcome to the universe.";
+let end = text.endsWith("welcome", 20);
+console.log(end);
