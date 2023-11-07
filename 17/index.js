78aff96cdfbe5123c8f3a342e61ba25d763494f9
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 13:58:07 +0530

    add example for string trim

--- 17/index.js
+++ 17/index.js
@@ -14,3 +14,7 @@ let text1 = "Hello";
 let text2 = "World";
 let text3 = text1.concat(" ", text2);
 console.log(text3);
+
+let text1 = "      Hello World!      ";
+let text2 = text1.trim();
+console.log(text2);
