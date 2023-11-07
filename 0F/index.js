88538db8473414763452302acf4a99608714991a
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 09:11:51 +0530

    add example for function borrowing

--- 0F/index.js
+++ 0F/index.js
@@ -18,3 +18,25 @@ let personTwo = {
 
 personOne.printName();
 personTwo.printName();
+
+//fiunction borrowing
+let personOne = {
+    firstName: "Balaji",
+    lastName: "Kr",
+
+    printName () {
+        console.log(this.firstName + " " +this.lastName);
+    }
+   
+}
+
+let personTwo = {
+    firstName: "Sachin",
+    lastName:"Tendulkar",
+
+    printName(){
+        console.log(this.firstName + " " + this.lastName);
+    }
+}
+
+personOne.printName.call(personTwo);
