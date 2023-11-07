90df6192f9feb008cb133ce85493f26caa5b1080
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 14:00:31 +0530

    add example for lastindexOf

--- 18/index.js
+++ 18/index.js
@@ -1,3 +1,7 @@
 let text = "Please locate where 'locate' occurs!";
 let index = text.indexOf("locate");
 console.log(index);
+
+let text = "Please locate where 'locate' occurs!";
+let index = text.lastIndexOf("locate");
+console.log(index);
