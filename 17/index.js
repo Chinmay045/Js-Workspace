6a15c11104f594cca0ca0daa9f51633087b68c2e
Chinmay <chinz.sandur07@gmail.com>
2023-11-07 13:57:36 +0530

    add example for string concat

--- 17/index.js
+++ 17/index.js
@@ -9,3 +9,8 @@ console.log(part);
 let text = "Please visit Microsoft!";
 let newText = text.replace("Microsoft", "W3Schools");
 console.log(newText);
+
+let text1 = "Hello";
+let text2 = "World";
+let text3 = text1.concat(" ", text2);
+console.log(text3);
