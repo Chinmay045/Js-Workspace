b871a6a3754bd7211678f0c93c51ca9d8b8c38d9
Chinmay <chinz.sandur07@gmail.com>
2023-11-08 20:25:28 +0530

    add example of operators in bigInt

--- 20/index.js
+++ 20/index.js
@@ -4,3 +4,8 @@ let y = 9999999999999999n;
 let x = BigInt(999999999999999);
 let type = typeof x;
 console.log(type);
+
+let x = 9007199254740995n;
+let y = 9007199254740995n;
+let z = x * y;
+console.log(z);
