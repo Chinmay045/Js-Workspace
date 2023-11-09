f4ded468f9fd9bd5f61929f74f254528fa8219aa
Chinmay <chinz.sandur07@gmail.com>
2023-11-09 16:17:40 +0530

    add example for math.random()

--- 21/index.js
+++ 21/index.js
@@ -1,3 +1,10 @@
 let x = Math.random();
 x = Math.floor(Math.random() * 10);
 console.log(x);
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+  }
+
+  getRndInteger();
