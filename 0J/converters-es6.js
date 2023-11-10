2a61a2accb477b284c40708e2828ca08477e3680
Chinmay <chinz.sandur07@gmail.com>
2023-11-10 14:12:16 +0530

    add example for Default Importing/Exporting Modules

--- 0J/converters-es6.js
+++ 0J/converters-es6.js
@@ -1,9 +1,20 @@
-function celciusToFarenheiet(celsius) {
+export function celciusToFarenheiet(celsius) {
     return celsius * (9 / 5) + 32;
 }
 
-const fahrenheitToCelsius = function (fahreheit) {
+export const fahrenheitToCelsius = function (fahreheit) {
     return (fahreheit - 32) * (5 / 9);
 };
 
-export { celciusToFarenheiet, fahrenheitToCelsius };
+
+export const milliToCenti = (millimeter) => {
+    return millimeter / 10;
+}
+
+export const centiToMeter = (centimeter) => {
+    return centimeter * 100;
+}
+
+const nanoMeter = 0.0000000001;
+
+export default nanoMeter;
--- 0J/water-limits-es6.js
+++ 0J/water-limits-es6.js
@@ -1,5 +1,4 @@
-import { celciusToFarenheiet, fahrenheitToCelsius } from './converters-es6.js';
-
+import * as resource from './converters-es6.js'
 const buttonElement = document.getElementById('button');
 console.log(buttonElement);
 buttonElement.addEventListener('click', clickMe);
@@ -7,8 +6,12 @@ buttonElement.addEventListener('click', clickMe);
 function clickMe() {
     let element = document.getElementById("container");
     console.log(element);
-    element.innerHTML = `<h1>Boiling point is :${celciusToFarenheiet(100)} 
-    & Freezing point is: ${celciusToFarenheiet(0)} </h1>`
+    element.innerHTML = `<h1>Boiling point is :${resource.celciusToFarenheiet(100)} 
+    & Freezing point is: ${resource.celciusToFarenheiet(0)} </h1>, <br><br>
+    100 milli meters is ${resource.milliToCenti(100)} centimeters, <br><br>
+    10000 centi meters is ${resource.centiToMeter(100000)} meters,<br><br>
+    One nanometers is ${resource.default} meters.
+    </h1>;`
 }
 
 console.log(`Water Boiling point is ${celciusToFarenheiet(100)} Degree Fahrenheit`);
