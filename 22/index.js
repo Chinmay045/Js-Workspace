e7447206617f4377a2e15b80e28f8e3fb6507fc3
Chinmay <chinz.sandur07@gmail.com>
2023-11-17 14:28:52 +0530

    add example of accessing arrays

--- 22/index.js
+++ 22/index.js
@@ -1,4 +1,6 @@
 // const cars = ["Saab", "Volvo", "BMW"];
 
-const cars = new Array("Saab", "Volvo", "BMW");
+// const cars = new Array("Saab", "Volvo", "BMW");
+const cars = ["Saab", "Volvo", "BMW"];
+let car = cars[0];
 console.log(cars);
