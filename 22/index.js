0669f93f72938da02ea9cb7450511cc9deacf1b4
Chinmay <chinz.sandur07@gmail.com>
2023-11-17 14:29:32 +0530

    add example for changing array element

--- 22/index.js
+++ 22/index.js
@@ -1,6 +1,8 @@
 // const cars = ["Saab", "Volvo", "BMW"];
 
 // const cars = new Array("Saab", "Volvo", "BMW");
+// const cars = ["Saab", "Volvo", "BMW"];
+// let car = cars[0];
 const cars = ["Saab", "Volvo", "BMW"];
-let car = cars[0];
+cars[0] = "Opel";
 console.log(cars);
