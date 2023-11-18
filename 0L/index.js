07672033927292b02d23d9d61345daaa775e2fd2
Chinmay <chinz.sandur07@gmail.com>
2023-11-18 11:25:46 +0530

    add create new promise

---
+++ 0L/index.js
@@ -0,0 +1,8 @@
+//Requirements:
+//1)Display keys of all profiles after 2 seconds
+//2)Display profiles of kishan after 3 sec(from one)
+//3)Display kishan's age after 2 seconds (from 2)
+//4)displaty kishans hobbies after 4 seconds (from 3)
+
+let goToHell = new Promise(keysPromiseFunction);//creating new promise
+
