0aa4f885ac24f17df2878dfab94c291c78573c63
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 09:35:33 +0530

    add example for array.pop()

--- 23/index.js
+++ 23/index.js
@@ -3,9 +3,14 @@ let size = fruits.length;
 console.log(size)
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
-let fru= fruits.toString();
+let fru = fruits.toString();
 console.log(fru);
 
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let fru = fruits.join(" * ");
 console.log(fru);
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.pop();
+console.log(fruits)
