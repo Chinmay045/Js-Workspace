66f9bcc2a0d9fb0a276e9a159460628183046152
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 13:38:29 +0530

    add example for callBack function

---
+++ 0N/index1.js
@@ -0,0 +1,21 @@
+function add(){
+    let res = 10 + 20;
+    console.log(res);
+}
+
+function  sub(){
+    let res = 10 - 5;
+    console.log(res);
+}
+
+function multiply(){
+    let res = x * y;
+    console.log(res); 
+}
+
+function show(anyFun){
+    console.log("Hello World");
+    anyFun();
+}
+
+show(add);
