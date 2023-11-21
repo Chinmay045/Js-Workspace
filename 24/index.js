0bca16b092cbf111f5d680380c52c2267797adb1
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 09:43:37 +0530

    add example for sorting numeric array

--- 24/index.js
+++ 24/index.js
@@ -5,3 +5,8 @@ const fruits = ["Banana", "Orange", "Apple", "Mango"];
 fruits.sort();
 fruits.reverse();
 console.log(fruits);
+
+
+const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+console.log(points)
