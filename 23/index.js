89bc470cd511d1dd90758dcc3851b220d11d0943
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 09:34:06 +0530

    add example for array to string

--- 23/index.js
+++ 23/index.js
@@ -1,3 +1,7 @@
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let size = fruits.length;
 console.log(size)
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let fru= fruits.toString();
+console.log(fru);
