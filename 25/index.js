e47ba6ed01d675bc0f43ff22ef4227ca91387ed7
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 13:50:18 +0530

    add example for reduce

--- 25/index.js
+++ 25/index.js
@@ -14,3 +14,12 @@ function myFunction(value, index, array) {
   return value > 18;
 }
 console.log(numbers)
+
+const numbers = [45, 4, 9, 16, 25];
+let sum = numbers.reduce(myFunction, 100);
+
+function myFunction(total, value) {
+  return total + value;
+}
+
+console.log(numbers)
