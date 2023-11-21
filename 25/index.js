a165af0f003e291c457ce30318e8d8f8e0d4a0ae
Chinmay <chinz.sandur07@gmail.com>
2023-11-21 13:49:34 +0530

    add example for some

--- 25/index.js
+++ 25/index.js
@@ -6,3 +6,11 @@ function myFunction(value, index, array) {
 }
 
 console.log(first)
+
+const numbers = [45, 4, 9, 16, 25];
+let someOver18 = numbers.some(myFunction);
+
+function myFunction(value, index, array) {
+  return value > 18;
+}
+console.log(numbers)
