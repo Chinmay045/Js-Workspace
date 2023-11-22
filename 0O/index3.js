f125b61b1562c4cad05f31a2ed574391f23cad98
Chinmay <chinz.sandur07@gmail.com>
2023-11-22 10:46:35 +0530

    add example for this keyword with prototype

---
+++ 0O/index3.js
@@ -0,0 +1,20 @@
+let animal = {
+    walk() {
+        if (!this.isSleeping) {
+            alert("I walk");
+        }
+    },
+    sleep() {
+        this.isSleeping = true;
+    }
+};
+
+let rabbit = {
+    name: "White Rabbit",
+    __proto__: animal
+};
+
+rabbit.sleep();
+
+alert(rabbit.isSleeping);
+alert(animal.isSleeping);
