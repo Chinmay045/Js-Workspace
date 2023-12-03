40042a5631e9442fe86ffcbfc02b42221c897ae1
Chinmay <chinz.sandur07@gmail.com>
2023-12-03 19:22:43 +0530

    add example of set

--- 26/index.js
+++ 26/index.js
@@ -3,4 +3,11 @@ const fruits = new Map([
     ["bananas", 300],
     ["oranges", 200]
   ]);
+  
+  const fruits = new Map();
+  
+  // Set Map Values
+  fruits.set("apples", 500);
+  fruits.set("bananas", 300);
+  fruits.set("oranges", 200);
   console.log(fruits);
