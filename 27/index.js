2a961e217de23f747a3f80e5b7ab9f76b92ac60e
Chinmay <chinz.sandur07@gmail.com>
2023-12-03 19:26:41 +0530

    add example of hoisting

--- 27/index.js
+++ 27/index.js
@@ -4,3 +4,9 @@ elem = document.getElementById("demo"); // Find an element
 elem.innerHTML = x;                     // Display x in the element
 
 var x; // Declare x
+
+var x = 5; // Initialize x
+var y = 7; // Initialize y
+
+elem = document.getElementById("demo"); // Find an element
+elem.innerHTML = x + " " + y;  
