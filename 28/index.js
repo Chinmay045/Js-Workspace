8fed1cbc290893e8963f16a345c1598a0f7e03dc
Chinmay <chinz.sandur07@gmail.com>
2023-12-03 19:29:00 +0530

    add example of usestrict

--- 28/index.js
+++ 28/index.js
@@ -1,2 +1,9 @@
 "use strict";
 x = 3.14;       // This will cause an error because x is not declared
+
+"use strict";
+myFunction();
+
+function myFunction() {
+  y = 3.14;   // This will also cause an error because y is not declared
+}
