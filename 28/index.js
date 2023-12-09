87ab74005cca5b9c70778ed29a982cc90f8a980d
Chinmay <chinz.sandur07@gmail.com>
2023-12-09 21:20:34 +0530

    update js logic

--- 28/index.js
+++ 28/index.js
@@ -7,3 +7,7 @@ myFunction();
 function myFunction() {
   y = 3.14;   // This will also cause an error because y is not declared
 }
+
+function myFunction() {
+  y = 3.14;   // This will also cause an error because y is not declared
+}
