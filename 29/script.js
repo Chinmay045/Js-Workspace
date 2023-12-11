70abb73591a311ca858ab1b301b0110680e665bb
Chinmay <chinz.sandur07@gmail.com>
2023-12-11 21:50:25 +0530

    add example for object

--- 29/script.js
+++ 29/script.js
@@ -1,8 +1,13 @@
-const person = {firstName:"John", lastName:"Doe", age:50, eyeColor:"blue"};
+const person = { firstName: "John", lastName: "Doe", age: 50, eyeColor: "blue" };
 const person = {
     firstName: "John",
     lastName: "Doe",
     age: 50,
     eyeColor: "blue"
-  };
+};
+const person = new Object();
+person.firstName = "John";
+person.lastName = "Doe";
+person.age = 50;
+person.eyeColor = "blue";
 console.log(person)
