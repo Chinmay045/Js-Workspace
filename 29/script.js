28cb1ee68745bdb576787f759ba70d0f55ff3ccd
Chinmay <chinz.sandur07@gmail.com>
2023-12-12 14:29:35 +0530

    add example for object constructor

--- 29/script.js
+++ 29/script.js
@@ -11,3 +11,13 @@ person.lastName = "Doe";
 person.age = 50;
 person.eyeColor = "blue";
 console.log(person)
+
+
+function Person(first, last, age, eye) {
+    this.firstName = first;
+    this.lastName = last;
+    this.age = age;
+    this.eyeColor = eye;
+  }
+
+  console.log(Person.firstName);
