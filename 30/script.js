c98e52a7a84683cc8528762a616d783e64f8d254
Chinmay <chinz.sandur07@gmail.com>
2023-12-12 14:40:37 +0530

    add example for iterables

--- 30/script.js
+++ 30/script.js
@@ -7,4 +7,7 @@ function Person(first, last, age, eyecolor) {
   
   Person.prototype.nationality = "English";
 
-  
+  for (const x of [1,2,3,4,5]) {
+    console.log(x);
+    // code block to be executed
+  }
