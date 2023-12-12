240e03f9a38ef6654eac86706f162484e14ece01
Chinmay <chinz.sandur07@gmail.com>
2023-12-12 15:38:00 +0530

    add example for call

--- 33/script.js
+++ 33/script.js
@@ -1,16 +1,18 @@
 let name = {
     firstName: "chinmay",
     lastName: "Sandur",
-    printfullName: function () {
-        console.log(this.firstName + " " + this.lastName);
-    }
+   
 };
 
-name.printfullName()
+let printfullName =  function () {
+    console.log(this.firstName + " " + this.lastName);
+}
+
+printfullName.call(name);
 
 let name2 = {
     firstName:"Sachin",
     lastName: "Tendlkar"
 }
 
-name.printfullName.call(name2);
+printfullName.call(name2);
