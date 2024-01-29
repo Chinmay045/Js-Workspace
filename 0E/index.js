b81847ab8e9f800a43c2ba9052abe2478e707646
Chinmay <chinz.sandur07@gmail.com>
2024-01-29 12:23:36 +0530

    add example for objects

--- 0E/index.js
+++ 0E/index.js
@@ -7,3 +7,15 @@ let dog = {
 
 console.log(dog.color);
 console.log(dog['responds to'])
+
+let baby = {
+    cat: 'kitten',
+    dog: 'puppy',
+    kangaroo: 'zoey',
+    'horse baby': 'fawn',
+    'baby duck': 'duckling',
+
+}
+
+console.log(baby['horse baby']);
+console.log(baby['baby duck']);
