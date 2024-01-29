baef5a238781f787914bfb580aff7c90852d75bc
Chinmay <chinz.sandur07@gmail.com>
2024-01-29 09:54:11 +0530

    add example for typeof and string interpolation

--- 0A/index.js
+++ 0A/index.js
@@ -16,13 +16,31 @@ console.log(Math.random());
 console.log(Math.ceil(45.677));
 console.log(Math.floor(45.6))
 
-let myName = 'chinmay';
-let age = 22;
-console.log('my name is ' + myName + ' and my age is ' + age);
-age = 23;
-console.log('my name is ' + myName + ' and my age is ' + age);
+// let myName = 'chinmay';
+// let age = 22;
+// console.log('my name is ' + myName + ' and my age is ' + age);
+// age = 23;
+// console.log('my name is ' + myName + ' and my age is ' + age);
 
 const age1 = 22;
 console.log(age1)
 age1 = 23;
 console.log(age1)
+
+
+var year = 2024;
+year++;
+year--;
+
+console.log(year)
+
+let myName = 'chinmay';
+let age = 22;
+console.log(`my name is  + ${myName} + ' and my age is ' + ${age}`);
+console.log('my name is ' + myName + ' and my age is ' + age);
+
+let foo = 'fooo';
+foo = 45;
+foo = 45.7;
+foo = 45 >46;
+console.log(typeof(foo))
