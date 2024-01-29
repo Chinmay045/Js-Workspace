2cafcb3defef7ff8aa32681551f7f8112a6cec20
Chinmay <chinz.sandur07@gmail.com>
2024-01-29 09:34:53 +0530

    add example for string methods

--- 0A/index.js
+++ 0A/index.js
@@ -7,3 +7,7 @@ console.log(b);
 console.log('hello ' + 'world');
 
 console.log('hello world'.length);
+
+console.log('hello'.startsWith('h'));
+console.log('world'.toLowerCase());
+console.log('world'.toUpperCase());
