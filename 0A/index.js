a157f62e935392d91d5adca192cecce0eef6f5a9
Chinmay <chinz.sandur07@gmail.com>
2024-01-29 09:37:13 +0530

    add example for built in objects

--- 0A/index.js
+++ 0A/index.js
@@ -11,3 +11,7 @@ console.log('hello world'.length);
 console.log('hello'.startsWith('h'));
 console.log('world'.toLowerCase());
 console.log('world'.toUpperCase());
+
+console.log(Math.random());
+console.log(Math.ceil(45.677));
+console.log(Math.floor(45.6))
