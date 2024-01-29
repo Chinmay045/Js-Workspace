0aa2914fe28134dcd79563c428fb4fa82c96d45e
Chinmay <chinz.sandur07@gmail.com>
2024-01-29 11:47:51 +0530

    add example for map and filter

--- 0D/index1.js
+++ 0D/index1.js
@@ -10,3 +10,28 @@ const sum = (params, a, b) => {
 
 sum(add, 43, 8);
 
+
+const fruitSalad = ['Banana', 'Kiwi', 'Orange', 'Mosumbi'];
+// const fruit = function (fruits) {
+//     console.log(`10 grams of ${fruits}`);
+// }
+// fruitSalad.forEach(fruit)
+fruitSalad.forEach((fruit) => {
+    console.log(`10 grams of ${fruit}`);
+})
+
+const numbers = [2, 3, 4, 5, 6];
+
+const squares = numbers.map((num) => {
+    let a = num * num;
+    return a;
+});
+console.log(squares);
+let filterEx = [23, 34, 56, 67, 'Banana', 'ornage', 'kiwi'];
+const events = filterEx.filter((sum) => {
+
+    return typeof sum == 'number';
+});
+
+console.log(events);
+
